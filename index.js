33ac4555ab34e72c2f76e95cf78bde19cb21be68
ian-sanchez <silkyhairbear@gmail.com>
2019-10-03 11:09:49 -0700

    ran into bug

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code away!
+const express = require('express')
+const postRouter = require('./server.js');
+
+const server = express();
+server.use(express.json());
+server.use('/api/posts', postRouter);
+
+server.listen(5555, () => console.log('Mr. server 5555'))
